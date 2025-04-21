ca2c9db2d9311744c657c783130b58e7d2399ffe
OsAfzal <178735553+OsAfzal@users.noreply.github.com>
2025-04-22 00:34:28 +0500

    Update Pakages_install.bat

--- Pakages_install.bat
+++ Pakages_install.bat
@@ -1,6 +1,13 @@
 @echo off
-echo Installing Python Pakages
+echo Installing Python Libraries...
 
 python -m pip install --upgrade pandas numpy matplotlib seaborn scikit-learn scipy statsmodels npstat jupyter
 python -m pip install --upgrade jupyterlab jupyterlab-git
+
+echo Python libraries Installed Successfully!
+
+echo Installing R Kernel... 
+R -e "install.packages(c('IRkernel' , 'rlang', 'lifecycle'm 'tidyverse'), repos='https://cran.rstudio.com/')"
+R -e "IRkernel::installspec(name = 'ir', displayname = 'R')"
+
 pause
