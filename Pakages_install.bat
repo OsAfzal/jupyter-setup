16215d68fcfef86b78bc13f22eecd7f487da5486
OsAfzal <178735553+OsAfzal@users.noreply.github.com>
2025-04-21 23:59:55 +0500

    Update Pakages_install.bat

--- Pakages_install.bat
+++ Pakages_install.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Installing Python Pakages
 
-python -m pip install --upgrade pandas numpy matplotlib seaborn scikit-learn scipy statsmodels jupyter jupyterlab jupyter-git
+python -m pip install --upgrade pandas numpy matplotlib seaborn scikit-learn scipy statsmodels npstat jupyter
 python -m pip install --upgrade jupyterlab jupyterlab-git
 pause
